346bf64c65b21f699db8ffe7b65e0ea9b7b7856d
DESKTOP-A1MG0NR\thunder <2864617610@qq.com>
2021-09-05 14:48:03 +0800

    demo 示例

--- demo/App.tsx
+++ demo/App.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import JsonView from "../src/index";
+
+import Switch from "./Switch";
 
 const data = [
     {
@@ -23,13 +25,47 @@ const data = [
 
 const App = () => {
 
+    const [indent, setIndent] = useState(4)
+    const [showArrayIndex, setShowArrayIndex] = useState(false)
+    const [singleQuote, setSingleQuote] = useState(false)
+    const [keyQuote, setKeyQuote] = useState(false)
+
+
+
     return (
-        <JsonView value={data}
-            showArrayIndex
-            // indent={4}
-            singleQuote
-        // keyQuote
-        />
+        <div style={{ padding: '10px 40px 40px' }}>
+
+            <section>
+                <div>
+                    当前缩进 : <input type="range" min={0} max={20} value={String(indent)} onChange={evt => setIndent(Number(evt.target.value))} />  {indent}
+                </div>
+
+                <div>
+                    是否显示数组索引：<Switch checked={showArrayIndex} onChange={setShowArrayIndex} />
+                </div>
+
+                <div>
+                    是否使用单引号(只针对值起作用)：<Switch checked={singleQuote} onChange={setSingleQuote} />
+                </div>
+
+                <div>
+                    对象的 key 是否显示引号：<Switch checked={keyQuote} onChange={setKeyQuote} />
+                </div>
+
+            </section>
+
+            <hr />
+
+            <section style={{ border: '1px solid #757575', padding: 10, borderRadius: 10 }}>
+                <JsonView value={data}
+                    showArrayIndex={showArrayIndex}
+                    indent={indent}
+                    singleQuote={singleQuote}
+                    keyQuote={keyQuote}
+                />
+            </section>
+        </div>
+
     )
 }
 
---
+++ demo/Switch.tsx
@@ -0,0 +1,30 @@
+import React, { CSSProperties, FC } from "react"
+
+const Switch: FC<{ checked: boolean, onChange: Function }> = ({ checked, onChange }) => {
+
+    const pos: CSSProperties = checked ? { right: 0 } : { left: 0, }
+
+    const style: CSSProperties = Object.assign({
+        transition: '0.2s',
+        display: 'inline-block', width: 25, height: '100%',
+        borderRadius: 100,
+        backgroundColor: checked ? '#3a54ff' : '#a9a9a9',
+        position: "absolute",
+        top: 0
+    }, pos)
+
+    return (
+        <button style={{
+            width: 60, height: 22, borderRadius: 100, border: 'none',
+            cursor: 'pointer',
+            position: 'relative',
+            verticalAlign: 'middle'
+        }}
+            onClick={() => onChange(!checked)}
+        >
+            <span style={style}></span>
+        </button>
+    )
+}
+
+export default Switch
